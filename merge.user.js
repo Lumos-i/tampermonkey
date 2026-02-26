c7f3712bc32993d12b56a29b8818e3b8a6ff761e
Lumos-i <112914450+Lumos-i@users.noreply.github.com>
2026-02-26 17:50:52 +0800

    3.0

--- merge.user.js
+++ merge.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dynamics Inline 标签优化检测
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  检测 content 字段中可合并的 inline 标签，提示用户是否修复
 // @author       zrq
 // @match        https://*.dynamics.com/main.aspx*
